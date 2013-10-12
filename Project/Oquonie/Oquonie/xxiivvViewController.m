70332c1e0e0ea21a353742b7a18dde25ad45c22e
Devine Lu Linvega <me@m0oo.com>
2013-10-12 23:36:53 +0900

    Event Cleanup

--- Project/Oquonie/Oquonie/xxiivvViewController.m
+++ Project/Oquonie/Oquonie/xxiivvViewController.m
@@ -50,6 +50,167 @@
 	userSpriteOrientationVertical = @"f";
 }
 
+# pragma mark Move -
+
+- (void) moveRouter :(int)posX :(int)posY :(UIButton *)sender
+{
+	NSLog(@"=====================");
+	
+	// Move disable timeout
+	[self moveDisable];
+	
+	int blocker = [self moveEvent:( userPositionX+posX ) :( userPositionY+posY )];
+	
+	// Detect event
+	if(blocker == 0){
+		// Update position
+		userPositionX += posX;
+		userPositionY += posY;
+		NSString *currentLocationString = worldNode[userLocation][[self flattenPosition:userPositionX :userPositionY]];
+		NSString *currentLocationEventKey = [self tileParser:currentLocationString :1];
+		NSString *currentLocationEventValue = [self tileParser:currentLocationString :2];
+		NSString *currentLocationEventData = [self tileParser:currentLocationString :3];
+		if( currentLocationEventKey ){
+			[self eventRouter:currentLocationEventKey:[currentLocationEventValue intValue]:currentLocationEventData];
+		}
+		// Sprite change
+		if( (long)sender.tag == 0 ){ userSpriteOrientationHorizontal = @"l"; userSpriteOrientationVertical = @"b"; }
+		if( (long)sender.tag == 1 ){ userSpriteOrientationHorizontal = @"r"; userSpriteOrientationVertical = @"b"; }
+		if( (long)sender.tag == 2 ){ userSpriteOrientationHorizontal = @"l"; userSpriteOrientationVertical = @"f"; }
+		if( (long)sender.tag == 3 ){ userSpriteOrientationHorizontal = @"r"; userSpriteOrientationVertical = @"f"; }
+		
+		[UIView beginAnimations: @"Fade In" context:nil];
+		[UIView setAnimationDuration:0.3];
+		[UIView setAnimationDelay:0];
+		self.userPlayer.frame = [self tileLocation:4:userPositionX:userPositionY];
+		[UIView commitAnimations];
+		
+		[self moveAnimation];
+	}
+	else{
+		[self moveCollide:posX:posY];
+	}
+	[self moveOrder];
+	
+	NSLog(@"> USER | Position: %d %d (%d)",userPositionX, userPositionY, [self flattenPosition:userPositionX :userPositionY]);
+}
+
+- (void) moveAnimation
+{
+	userSpriteState = @"walk";
+	self.userPlayerChar.image = [UIImage imageNamed: [NSString stringWithFormat:@"%@", [self templateSpriteName:@"1"] ] ];
+	[NSTimer scheduledTimerWithTimeInterval:0.15 target:self selector:@selector(animator1) userInfo:nil repeats:NO];
+	[NSTimer scheduledTimerWithTimeInterval:0.30 target:self selector:@selector(animator2) userInfo:nil repeats:NO];
+}
+
+- (void) moveEnable
+{
+	self.moveBL.hidden = NO;
+	self.moveBR.hidden = NO;
+	self.moveTL.hidden = NO;
+	self.moveTL.hidden = NO;
+}
+
+- (void) moveDisable
+{
+	self.moveBL.hidden = YES;
+	self.moveBR.hidden = YES;
+	self.moveTL.hidden = YES;
+	self.moveTL.hidden = YES;
+	[NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(moveEnable) userInfo:nil repeats:NO];
+}
+
+- (void) moveOrder
+{
+	userPositionZ = ( userPositionX + userPositionY );
+	
+	if( userPositionZ > 0 ){
+		[self.view bringSubviewToFront:self.blocker1];
+	}
+	else{
+		[self.view bringSubviewToFront:self.userPlayer];
+	}
+}
+
+- (int) moveEvent :(int)posX :(int)posY
+{
+	if( [worldNode[userLocation][[self flattenPosition:posX :posY]] intValue] == 0 ){
+		NSLog(@"> EVNT | Blocked: %d %d (no ground)", posX, posY);
+		[self moveCollide:posX:posY];
+		return 1;
+	}
+	return 0;
+}
+
+- (void) moveCollide :(int)posX :(int)posY
+{
+	for (UIView *subview in [self.view subviews]) {
+		if( subview.tag == 300 ){
+			CGRect origin = subview.frame;
+			subview.frame = CGRectOffset(subview.frame, 0, -2);
+			[UIView beginAnimations: @"Fade In" context:nil];
+			[UIView setAnimationDuration:0.3];
+			[UIView setAnimationDelay:0];
+			subview.frame = origin;
+			[UIView commitAnimations];
+		}
+	}
+	self.userPlayerChar.image = [UIImage imageNamed: [NSString stringWithFormat:@"%@", [self templateSpriteName:@""] ] ];
+	[self moveCollideAnimate:posX:posY];
+}
+
+-(void)moveCollideAnimate :(int)posX :(int)posY {
+	CGRect userOrigin = self.userPlayer.frame;
+	if( (posX == -1 && posY == 0) || (posX == 0 && posY == 1) ){ self.userPlayer.frame = CGRectOffset(self.userPlayer.frame, 2, 0); }
+	if( (posX == 0 && posY == -1) || (posX == 1 && posY == 0) ){ self.userPlayer.frame = CGRectOffset(self.userPlayer.frame, -2, 0); }
+	[UIView beginAnimations: @"Fade In" context:nil]; [UIView setAnimationDuration:0.3];
+	self.userPlayer.frame = userOrigin;
+	[UIView commitAnimations];
+}
+
+# pragma mark Event Router -
+
+-(void)eventRouter :(NSString*)eventType :(int)eventId :(NSString*)eventData {
+	
+	NSLog(@"> USER | Event: %@ -> %d", eventType, eventId );
+	
+	if ([eventType isEqualToString:@"warp"]) {
+		[self eventWarp:eventId:eventData];
+	}
+	
+}
+
+- (void)eventWarp :(int)eventId :(NSString*)eventData
+{
+	
+	NSArray* array = [eventData componentsSeparatedByString: @","];
+	int x = [[array objectAtIndex: 0] intValue];
+	int y = [[array objectAtIndex: 1] intValue];
+	
+	self.userPlayerChar.alpha = 0;
+	
+	userPositionX = x;
+	userPositionY = y;
+	userLocation = eventId;
+	self.userPlayer.frame = [self tileLocation:4:userPositionX:userPositionY];
+	[self roomStart];
+	[self templateRoomAnimation];
+	
+}
+
+# pragma mark Speak -
+
+-(void)eventSpeak:(NSString*)eventDialog {
+	
+	NSString *textCharacter1 = [eventDialog substringWithRange: NSMakeRange(0, 1) ];
+	NSString *textCharacter2 = [eventDialog substringWithRange: NSMakeRange(1, 1) ];
+	NSString *textCharacter3 = [eventDialog substringWithRange: NSMakeRange(2, 1) ];
+	NSString *textCharacter4 = [eventDialog substringWithRange: NSMakeRange(3, 1) ];
+	
+	NSLog(@"> EVNT | Saying:'%@ %@ %@ %@'", textCharacter1, textCharacter2, textCharacter3, textCharacter4);
+
+}
+
 # pragma mark Misc -
 
 - (void) animator1
@@ -141,220 +302,10 @@
 	if( posX == 1 && posY == 2 ){ return CGRectMake(centerW-(tileW/2)*-1, centerH+(tileH*0.5)*-0.79, tileW, tileH); }
 	
 	NSLog(@"error");
-
+	
 	return CGRectMake(0, 0, 100, 100);
 }
 
-# pragma mark Move -
-
-- (void) moveRouter :(int)posX :(int)posY :(UIButton *)sender
-{
-	NSLog(@"=====================");
-	
-	int blocker = [self moveEvent:( userPositionX+posX ) :( userPositionY+posY )];
-	
-	// Detect event
-	if(blocker == 0){
-		
-		// Update position
-		userPositionX += posX;
-		userPositionY += posY;
-		
-		NSString *currentLocationString = worldNode[userLocation][[self flattenPosition:userPositionX :userPositionY]];
-		NSString *currentLocationEventKey = [self tileParser:currentLocationString :1];
-		NSString *currentLocationEventValue = [self tileParser:currentLocationString :2];
-		NSString *currentLocationEventData = [self tileParser:currentLocationString :3];
-		if( currentLocationEventKey ){
-			[self eventRouter:currentLocationEventKey:[currentLocationEventValue intValue]:currentLocationEventData];
-		}
-		
-	}
-	
-	// Move timeout
-	[self moveDisable];
-	
-	// Sprite change
-	if( (long)sender.tag == 0 ){ userSpriteOrientationHorizontal = @"l"; userSpriteOrientationVertical = @"b"; }
-	if( (long)sender.tag == 1 ){ userSpriteOrientationHorizontal = @"r"; userSpriteOrientationVertical = @"b"; }
-	if( (long)sender.tag == 2 ){ userSpriteOrientationHorizontal = @"l"; userSpriteOrientationVertical = @"f"; }
-	if( (long)sender.tag == 3 ){ userSpriteOrientationHorizontal = @"r"; userSpriteOrientationVertical = @"f"; }
-	
-	[UIView beginAnimations: @"Fade In" context:nil];
-	[UIView setAnimationDuration:0.3];
-	[UIView setAnimationDelay:0];
-	self.userPlayer.frame = [self tileLocation:4:userPositionX:userPositionY];
-	[UIView commitAnimations];
-	
-	if(blocker == 0){
-		[self moveAnimation];
-	}
-	
-	if(blocker == 1){
-		[self moveCollide:posX:posY];
-	}
-	
-	[self moveOrder];
-	
-	NSLog(@"> USER | Position: %d %d (%d)",userPositionX, userPositionY, [self flattenPosition:userPositionX :userPositionY]);
-}
-
-- (void) moveAnimation
-{
-	userSpriteState = @"walk";
-	self.userPlayerChar.image = [UIImage imageNamed: [NSString stringWithFormat:@"%@", [self templateSpriteName:@"1"] ] ];
-	[NSTimer scheduledTimerWithTimeInterval:0.15 target:self selector:@selector(animator1) userInfo:nil repeats:NO];
-	[NSTimer scheduledTimerWithTimeInterval:0.30 target:self selector:@selector(animator2) userInfo:nil repeats:NO];
-}
-
-- (void) moveEnable
-{
-	self.moveBL.hidden = NO;
-	self.moveBR.hidden = NO;
-	self.moveTL.hidden = NO;
-	self.moveTL.hidden = NO;
-}
-
-- (void) moveDisable
-{
-	self.moveBL.hidden = YES;
-	self.moveBR.hidden = YES;
-	self.moveTL.hidden = YES;
-	self.moveTL.hidden = YES;
-	[NSTimer scheduledTimerWithTimeInterval:0.2 target:self selector:@selector(moveEnable) userInfo:nil repeats:NO];
-}
-
-- (void) moveOrder
-{
-	userPositionZ = ( userPositionX + userPositionY );
-	
-	if( userPositionZ > 0 ){
-		[self.view bringSubviewToFront:self.blocker1];
-	}
-	else{
-		[self.view bringSubviewToFront:self.userPlayer];
-	}
-}
-
-- (int) moveEvent :(int)posX :(int)posY
-{
-	// No End
-	if( [worldNode[userLocation][[self flattenPosition:posX :posY]] intValue] == 0 ){
-		NSLog(@"> EVNT | Blocked: %d %d (no ground)", posX, posY);
-		[self moveCollide:posX:posY];
-		return 1;
-	}
-	
-	return 0;
-}
-
-- (void) moveCollide :(int)posX :(int)posY
-{
-	for (UIView *subview in [self.view subviews]) {
-		if( subview.tag == 300 ){
-			CGRect origin = subview.frame;
-			subview.frame = CGRectOffset(subview.frame, 0, -2);
-			[UIView beginAnimations: @"Fade In" context:nil];
-			[UIView setAnimationDuration:0.3];
-			[UIView setAnimationDelay:0];
-			subview.frame = origin;
-			[UIView commitAnimations];
-		}
-	}
-	self.userPlayerChar.image = [UIImage imageNamed: [NSString stringWithFormat:@"%@", [self templateSpriteName:@""] ] ];
-	[self moveCollideAnimate:posX:posY];
-}
-
--(void)moveCollideAnimate :(int)posX :(int)posY {
-	CGRect userOrigin = self.userPlayer.frame;
-	if( (posX == -1 && posY == 0) || (posX == 0 && posY == 1) ){ self.userPlayer.frame = CGRectOffset(self.userPlayer.frame, 2, 0); }
-	if( (posX == 0 && posY == -1) || (posX == 1 && posY == 0) ){ self.userPlayer.frame = CGRectOffset(self.userPlayer.frame, -2, 0); }
-	[UIView beginAnimations: @"Fade In" context:nil]; [UIView setAnimationDuration:0.3];
-	self.userPlayer.frame = userOrigin;
-	[UIView commitAnimations];
-}
-
-# pragma mark Event Router -
-
--(void)eventRouter :(NSString*)eventType :(int)eventId :(NSString*)eventData {
-	
-	NSLog(@"> USER | Event: %@ -> %d", eventType, eventId );
-	
-	if ([eventType isEqualToString:@"warp"]) {
-		[self eventWarp:eventId:eventData];
-	}
-	
-
-}
-
-- (void)eventWarp :(int)eventId :(NSString*)eventData
-{
-	
-	NSArray* array = [eventData componentsSeparatedByString: @","];
-	int x = [[array objectAtIndex: 0] intValue];
-	int y = [[array objectAtIndex: 1] intValue];
-	
-	self.userPlayerChar.alpha = 0;
-	
-	userPositionX = x;
-	userPositionY = y;
-	userLocation = eventId;
-	self.userPlayer.frame = [self tileLocation:4:userPositionX:userPositionY];
-	[self roomStart];
-	[self templateRoomAnimation];
-	
-}
-
--(void)eventDialog:(int)dialogId {
-	
-	NSString* eventDialog = worldEventDialog[1][userSpriteCharId][1];
-	int eventSpell = [worldEventDialog[1][userSpriteCharId][0] intValue];
-	
-	if(eventDialog > 0){
-		[self eventSpeak:eventDialog];
-	}
-	
-	if(eventSpell > 0){
-		[self eventSpell:eventSpell];
-	}
-
-}
-
--(void)eventSpell:(int)eventSpell {
-	
-	NSLog(@"> EVNT | Spell #%d", eventSpell);
-	
-}
-
-# pragma mark Speak -
-
--(void)eventSpeak:(NSString*)eventDialog {
-	
-	NSString *textCharacter1 = [eventDialog substringWithRange: NSMakeRange(0, 1) ];
-	NSString *textCharacter2 = [eventDialog substringWithRange: NSMakeRange(1, 1) ];
-	NSString *textCharacter3 = [eventDialog substringWithRange: NSMakeRange(2, 1) ];
-	NSString *textCharacter4 = [eventDialog substringWithRange: NSMakeRange(3, 1) ];
-	
-	[self eventSpeakShow];
-	
-	NSLog(@"> EVNT | Saying:'%@ %@ %@ %@'", textCharacter1, textCharacter2, textCharacter3, textCharacter4);
-
-}
-
-- (void)eventSpeakShow {
-	
-	[UIView beginAnimations: @"Fade In" context:nil];
-	[UIView setAnimationDuration:1];
-	[UIView setAnimationDelay:0];
-	
-	[UIView commitAnimations];
-	
-}
-
-- (void)eventSpeakHide {
-	
-	
-	
-}
 
 -(int)flattenPosition :(int)x :(int)y {
 	
@@ -387,7 +338,6 @@
 	if(x==-2&& y== 1){ return 20; }
 	
 	return 1;
-	
 }
 
 
