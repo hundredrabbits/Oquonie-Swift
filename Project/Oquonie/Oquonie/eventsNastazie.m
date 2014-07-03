f557a05d31c0d9910d35c8a2cde03ce1c2d59035
Devine Lu Linvega <me@m0oo.com>
2014-07-03 11:33:49 -0400

    Begin puzzle building

--- Project/Oquonie/Oquonie/eventsNastazie.m
+++ Project/Oquonie/Oquonie/eventsNastazie.m
@@ -26,4 +26,100 @@
 	return @"";
 }
 
+-(NSString*)event_nastazieNemedique1 :(NSString*)option
+{
+	// Event Identifier
+	NSString * eventSpellId		= @"nastazieNemedique1";
+	NSString * eventCharacter	= @"nemedique";
+	NSString * eventSpriteId	= eventNemedique;
+	int		   eventSpell		= spellNemedique;
+	NSString * eventLetter		= letterNemedique;
+	
+	NSString * eventDialogSpell = dialogGainSpell(eventLetter);
+	// Broadcast Notification
+	if([option isEqualToString:@"postNotification"]){
+		if( ![self eventSpellCheck:eventSpellId] && userCharacter != eventSpell ){
+			return eventLetter;
+		}
+		return @"";
+	}
+	// Broadcast Event Sprite Change
+	if([option isEqualToString:@"postUpdate"]){
+		return @"";
+	}
+	// Dialogs
+	if( eventSpell == userCharacter)				{ [self eventDialog:dialogHaveCharacter:eventSpriteId]; }
+    else if( [self eventSpellCheck:eventSpellId] )	{ [self eventDialog:dialogHaveSpell:eventSpriteId]; }
+    else											{ [self eventDialog:eventDialogSpell:eventSpriteId]; }
+	// Spell
+	[self eventSpellAdd:eventSpellId:eventSpell];
+	[self audioDialogPlayer:eventCharacter];
+	
+	return @"";
+}
+
+-(NSString*)event_nastazieNemedique2 :(NSString*)option
+{
+	// Event Identifier
+	NSString * eventSpellId		= @"nastazieNemedique2";
+	NSString * eventCharacter	= @"nemedique";
+	NSString * eventSpriteId	= eventNemedique;
+	int		   eventSpell		= spellNemedique;
+	NSString * eventLetter		= letterNemedique;
+	
+	NSString * eventDialogSpell = dialogGainSpell(eventLetter);
+	// Broadcast Notification
+	if([option isEqualToString:@"postNotification"]){
+		if( ![self eventSpellCheck:eventSpellId] && userCharacter != eventSpell ){
+			return eventLetter;
+		}
+		return @"";
+	}
+	// Broadcast Event Sprite Change
+	if([option isEqualToString:@"postUpdate"]){
+		return @"";
+	}
+	// Dialogs
+	if( eventSpell == userCharacter)				{ [self eventDialog:dialogHaveCharacter:eventSpriteId]; }
+    else if( [self eventSpellCheck:eventSpellId] )	{ [self eventDialog:dialogHaveSpell:eventSpriteId]; }
+    else											{ [self eventDialog:eventDialogSpell:eventSpriteId]; }
+	// Spell
+	[self eventSpellAdd:eventSpellId:eventSpell];
+	[self audioDialogPlayer:eventCharacter];
+	
+	return @"";
+}
+
+-(NSString*)event_nastazieNemedique3 :(NSString*)option
+{
+	// Event Identifier
+	NSString * eventSpellId		= @"nastazieNemedique3";
+	NSString * eventCharacter	= @"nemedique";
+	NSString * eventSpriteId	= eventNemedique;
+	int		   eventSpell		= spellNemedique;
+	NSString * eventLetter		= letterNemedique;
+	
+	NSString * eventDialogSpell = dialogGainSpell(eventLetter);
+	// Broadcast Notification
+	if([option isEqualToString:@"postNotification"]){
+		if( ![self eventSpellCheck:eventSpellId] && userCharacter != eventSpell ){
+			return eventLetter;
+		}
+		return @"";
+	}
+	// Broadcast Event Sprite Change
+	if([option isEqualToString:@"postUpdate"]){
+		return @"";
+	}
+	// Dialogs
+	if( eventSpell == userCharacter)				{ [self eventDialog:dialogHaveCharacter:eventSpriteId]; }
+    else if( [self eventSpellCheck:eventSpellId] )	{ [self eventDialog:dialogHaveSpell:eventSpriteId]; }
+    else											{ [self eventDialog:eventDialogSpell:eventSpriteId]; }
+	// Spell
+	[self eventSpellAdd:eventSpellId:eventSpell];
+	[self audioDialogPlayer:eventCharacter];
+	
+	return @"";
+}
+
 @end
